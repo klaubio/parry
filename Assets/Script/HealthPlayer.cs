ef5631ab859a018befa04e5bf043a641cb2be8c3
klaubio <renatoalvarezxd78@gmail.com>
2024-05-03 17:50:35 -0400

    Health

---
+++ Assets/Script/HealthPlayer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPlayer : MonoBehaviour
+{
+    [SerializeField] private float pHealth = 125f;
+
+
+
+}
